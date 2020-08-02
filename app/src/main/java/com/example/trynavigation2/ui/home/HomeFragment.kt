4941d69cf04273ac8edd6723bd9744b564dbde0d
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-08-02 14:57:07 +0900

    ボタンで遷移

--- app/src/main/java/com/example/trynavigation2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trynavigation2/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.trynavigation2.R
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -23,6 +25,10 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             text_home.text = it
         })
+        val btn2dash = root.findViewById<Button>(R.id.button1)
+        val btn2notif = root.findViewById<Button>(R.id.button2)
+        btn2dash.setOnClickListener { findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard) }
+        btn2notif.setOnClickListener { findNavController().navigate(R.id.action_navigation_home_to_navigation_notifications) }
         return root
     }
 }
