65a714048b06fb2a43f1482cdc3d880f11819b86
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-08-02 13:50:42 +0900

    テンプレートの古い実装を修正

--- app/src/main/java/com/example/trynavigation2/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/trynavigation2/ui/notifications/NotificationsFragment.kt
@@ -4,27 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.trynavigation2.R
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private val notificationsViewModel: NotificationsViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            text_notifications.text = it
         })
         return root
     }
